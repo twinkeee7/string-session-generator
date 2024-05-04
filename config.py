0960783bd34857b8e6cf006a100f4e667da7c5df
twinkeee7 <167674604+twinkeee7@users.noreply.github.com>
2024-05-04 15:00:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("7062539103"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ultroidofficial_chat")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Riseeuniverse")
